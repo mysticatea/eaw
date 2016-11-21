fdd8ee60e32adf3893bc2de02f1bc155cd368bb4
Toru Nagashima <star.ctor@gmail.com>
2016-11-21 21:34:35 +0900

    New: `split` function

--- index.js
+++ index.js
@@ -12,4 +12,5 @@
 module.exports = {
     getWidth: require("./lib/get-width"),
     isNarrowCharacter: require("./lib/is-narrow-character"),
+    split: require("./lib/split"),
 }
---
+++ test/split.js
@@ -0,0 +1,96 @@
+/**
+ * @author Toru Nagashima <https://github.com/mysticatea>
+ * @copyright 2016 Toru Nagashima. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+"use strict"
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const assert = require("assert")
+const split = require("../").split
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+describe("split function", () => {
+    const patterns = [
+        [
+            "Hello",
+            [["Hel", "lo"], ["Hell", "o"], ["Hello"]], // x2.0; by 3, 4, 5
+            [["Hel", "lo"], ["Hell", "o"], ["Hello"]], // x1.5; by 3, 4, 5
+        ],
+        [
+            "アイウエオ",
+            [["ア", "イ", "ウ", "エ", "オ"], ["アイ", "ウエ", "オ"], ["アイ", "ウエ", "オ"]],
+            [["アイ", "ウエ", "オ"], ["アイ", "ウエ", "オ"], ["アイウ", "エオ"]],
+        ],
+        [
+            "ｱｲｳｴｵ",
+            [["ｱｲｳ", "ｴｵ"], ["ｱｲｳｴ", "ｵ"], ["ｱｲｳｴｵ"]],
+            [["ｱｲｳ", "ｴｵ"], ["ｱｲｳｴ", "ｵ"], ["ｱｲｳｴｵ"]],
+        ],
+        [
+            "Hello, 世界",
+            [["Hel", "lo,", " 世", "界"], ["Hell", "o, ", "世界"], ["Hello", ", 世", "界"]],
+            [["Hel", "lo,", " 世", "界"], ["Hell", "o, ", "世界"], ["Hello", ", 世界"]],
+        ],
+        [
+            "ひとつ､ふたつ｡",
+            [["ひ", "と", "つ､", "ふ", "た", "つ｡"], ["ひと", "つ､", "ふた", "つ｡"], ["ひと", "つ､ふ", "たつ｡"]],
+            [["ひと", "つ､", "ふた", "つ｡"], ["ひと", "つ､ふ", "たつ｡"], ["ひとつ", "､ふた", "つ｡"]],
+        ],
+    ]
+
+    for (const x of patterns) {
+        for (let i = 0; i < x[1].length; ++i) {
+            it(`should return ${JSON.stringify(x[1][i])} if ("${x[0]}", ${3 + i}) was given.`, () => {
+                assert.deepEqual(split(x[0], 3 + i), x[1][i])
+            })
+        }
+    }
+
+    for (let i = 0; i < patterns[0][1].length; ++i) {
+        it(`should return ${
+            JSON.stringify([].concat.apply([], patterns.map(x => x[1][i])))
+        } if ("${
+            JSON.stringify(patterns.map(x => x[0]))
+        }", ${3 + i}) was given.`, () => {
+            assert.deepEqual(
+                split(
+                    patterns.map(x => x[0]),
+                    3 + i
+                ),
+                [].concat.apply([], patterns.map(x => x[1][i]))
+            )
+        })
+    }
+
+    for (const x of patterns) {
+        for (let i = 0; i < x[2].length; ++i) {
+            it(`should return ${JSON.stringify(x[2][i])} if ("${x[0]}", ${3 + i}, 1.5) was given.`, () => {
+                assert.deepEqual(split(x[0], 3 + i, 1.5), x[2][i])
+            })
+        }
+    }
+
+    for (let i = 0; i < patterns[0][2].length; ++i) {
+        it(`should return ${
+            JSON.stringify([].concat.apply([], patterns.map(x => x[2][i])))
+        } if ("${
+            JSON.stringify(patterns.map(x => x[0]))
+        }", ${3 + i}, 1.5) was given.`, () => {
+            assert.deepEqual(
+                split(
+                    patterns.map(x => x[0]),
+                    3 + i,
+                    1.5
+                ),
+                [].concat.apply([], patterns.map(x => x[2][i]))
+            )
+        })
+    }
+})
