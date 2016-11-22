bb32f23619a34ba76e0578a138f02001199262b0
Toru Nagashima <star.ctor@gmail.com>
2016-11-22 14:17:35 +0900

    New: the entry point for browsers.
    This entry file does not include streams.

---
+++ browser.js
@@ -0,0 +1,16 @@
+/**
+ * @author Toru Nagashima <https://github.com/mysticatea>
+ * @copyright 2016 Toru Nagashima. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+"use strict"
+
+//------------------------------------------------------------------------------
+// Requirements and exports
+//------------------------------------------------------------------------------
+
+module.exports = {
+    getWidth: require("./lib/get-width"),
+    isNarrowCharacter: require("./lib/is-narrow-character"),
+    split: require("./lib/split"),
+}
